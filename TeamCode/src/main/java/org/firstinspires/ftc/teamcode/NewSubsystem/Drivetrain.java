613a6e42e444ba2cfc0063941ad4d949773c4429
Jacob Marinas <46766490+JayZeeKay@users.noreply.github.com>
2020-01-07 16:16:59 -0800

    Update Drivetrain.java
    - Added init(), hasEnc(),

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewSubsystem/Drivetrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewSubsystem/Drivetrain.java
@@ -12,6 +12,29 @@ public class Drivetrain {
         frontLeft = fL;
         backRight = bR;
         backLeft = bL;
+        
+        init();
+    }
+    
+    public void init(){
+        frontRight.setDirection(DcMotor.Direction.REVERSE);
+        frontLeft.setDirection(DcMotor.Direction.FORWARDS);
+        backRight.setDirection(DcMotor.Direction.REVERSE);
+        backLeft.setDirection(DcMotor.Direction.FORWARDS);
+    }
+    
+    public void hasEnc(bool has){
+        if (has){
+            frontRight.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+            frontLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+            backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+            backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODERS);
+        } else {
+            frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+            frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+            backRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+            backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODERS);
+        }
     }
 
     public void setPower(double power){
@@ -28,4 +51,10 @@ public class Drivetrain {
         backRight.setPower(power);
         backLeft.setPower(power);
     }
+    
+    public void backwards(){
+        frontRight.setPower(power);
+        frontLeft.setPower(power);
+        backRight.setPower(power);
+        backLeft.setPower(power);
 }

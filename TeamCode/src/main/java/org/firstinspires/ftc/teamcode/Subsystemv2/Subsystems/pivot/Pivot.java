d38a2400a6e80c13ca560903130698db37719fb7
Jacob Marinas <46766490+JayZeeKay@users.noreply.github.com>
2020-02-11 09:09:36 -0800

    Coding on the phone

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystemv2/Subsystems/pivot/Pivot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Subsystemv2/Subsystems/pivot/Pivot.java
@@ -1,4 +1,39 @@
 package org.firstinspires.ftc.teamcode.Subsystemv2.subsystems.pivot;
 
-public class Pivot {
+public class Pivot extends Constants {
+
+    private final DcMotor leftPivot, rightPivot;
+    private double power, currentSpeed, goalSpeed, speedRate;
+    private Status pivotStatus = Status.NEUTRAL;
+
+    public Pivot(DcMotor lP, DcMotor rP) {
+        this.leftPivot = lP;
+        this.rightPivot = rP;
+        
+        init();
+    }
+
+    private void init() {
+        leftPivot.setDirection();
+        rightPivot.setDirection();
+
+        power = STOP;
+    }
+
+    public void setPivot(double lPPower, double rPPower) {
+       leftPivot.setPower(lPPower);
+       rightPivot.setPower(rPPower);
+    }
+
+    public
+        pivotStatus = Status.UP;
+    }
+
+    public void down() {
+        leftPivot.setPower(-power);
+        rightPivot.setPower(-power);
+    }
+
+    public void stop() {
+        leftPivot.
 }
